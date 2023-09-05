a255fbf4f28abc453d158831e8f041910eab54af
pbokolo <dev.pbokolo@gmail.com>
2023-09-05 09:11:19 +0200

    Managing app lifecycle events

--- main.js
+++ main.js
@@ -5,4 +5,17 @@ const createWindow = () => {
   win.loadFile("index.html");
 };
 
-app.whenReady().then(() => createWindow());
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", () => {
+    // Opens the window if none are open(macos)
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    // Quits the app if the platform is not darwin(macos)
+    app.quit();
+  }
+});
