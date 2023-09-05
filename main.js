1560655c0c815991210b8a867f942904c5bd0377
pbokolo <dev.pbokolo@gmail.com>
2023-09-05 09:06:34 +0200

    Setting up the app.js file

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello world! This is my new journey with electron👋");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({ width: 800, height: 600 });
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => createWindow());
